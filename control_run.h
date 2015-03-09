833648832b188d5dc2bbee42b4555000eb5488eb
Facundo M. Gilles <fmgilles@gmail.com>
2015-03-09 14:20:22 -0300

    se borra el modulo_kinsolparam.f90 no es necesario. Se incorpora control_run.h.

--- control_run.h
+++ control_run.h
@@ -1,6 +1,14 @@
 /* esto es un comentario */
 
-#define CHAIN 1
-
+#define CHAIN 1  /* 0 - Without polymer chains
+                    1 - With polymer chains (monolayers)
+                    2 - Grafted polymer chains
+                    
+                 */
+#undef geometry 1 /* Select geometry 0 - Flat surface?
+                                     1 - Long Cylinder (1D)
+                                     2 - Square section nanochannel 
+                                     3 - Short 2D Cylinder 
+                  */
 #undef VDW 
 
